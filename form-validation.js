f3187288b33d874529137448c1b7ea37d827827e
Andrew Ryan <aryan17@mail.csuchico.edu>
2016-10-10 16:38:33 -0700

    username and password checks done

--- form-validation.js
+++ form-validation.js
@@ -3,17 +3,28 @@ function validate(){
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
 
+  var userLength = userEntered.length;
+  if(userLength >= 6){
+    //document.getElementById("usernameGroup").innerHTML="Good username.";
+    document.getElementById("usernameGroup").classList.add("has-success");
+  }
+  else{
   //Show message that there is an error with the username...
   document.getElementById("usernameError").innerHTML="Bad username.";
   document.getElementById("usernameError").classList.remove("hidden-message");
   document.getElementById("usernameError").classList.add("shown-message");
   //Turn the username items red
   document.getElementById("usernameGroup").classList.add("has-error");
-
+  }
+  if(passEntered == "password"){
   //Show message that there is an error with the password...
   document.getElementById("passwordError").innerHTML="Bad password.";
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
   document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else
+    document.getElementById("passwordGroup").classList.add("has-success");
+
 }
